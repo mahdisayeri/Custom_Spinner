dcd7bf142a83d17f89d2e709c48c53d2755dca3f
mahdi sayeri <mahdisayeri@yahoo.com>
2019-08-12 20:56:47 +0430

    set adapter on spinner and complete main activity

--- app/src/main/java/ir/tokaterm/customspinner/MainActivity.java
+++ app/src/main/java/ir/tokaterm/customspinner/MainActivity.java
@@ -2,15 +2,39 @@ package ir.tokaterm.customspinner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    String[] flags;
-    int[] name;
+    int[] flags={R.drawable.germany,R.drawable.unitedstates,R.drawable.australia};
+    String[] name={"Germany","United_Stats","Australia"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        flags={R.drawable.germany,R.drawable.united_states,R.drawable.australia};
+
+        Spinner spinner=findViewById(R.id.main_spinner);
+
+        CustomAdapter customAdapter=new CustomAdapter(getApplicationContext(),flags,name);
+        spinner.setAdapter(customAdapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "You Select Position: "+position+" "+name[position], Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+
     }
 }
